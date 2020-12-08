484c250239eca373660606f9403e78dead1ab06f
John DoeVini <vinichimania98@gmail.com>
2020-12-08 11:12:00 +0530

    sol2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-app.get("/home",(req,res)=>{
+app.get("",(req,res)=>{
     res.send("Hello world!");
 });
 
